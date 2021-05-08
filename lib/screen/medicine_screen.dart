da2db02e80d75c78842427e1cbebca7606b035b0
Kamil <kamilanwar2001@gmail.com>
2021-05-08 11:53:10 +0530

    spinner change 2

--- lib/screen/medicine_screen.dart
+++ lib/screen/medicine_screen.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 
 import 'package:covimitr/services/data.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:http/http.dart';
 import 'package:intl/intl.dart';
 
@@ -63,7 +64,11 @@ class _MedicineScreenState extends State<MedicineScreen> {
                   } else {
                     if (oxySnapshot.connectionState ==
                         ConnectionState.waiting) {
-                      return Center(child: CircularProgressIndicator());
+                      return Center(
+                          child: SpinKitFoldingCube(
+                        color: Color.fromRGBO(71, 20, 61, 0.9),
+                        size: 30,
+                      ));
                     } else {
                       Response resOxy = oxySnapshot.data;
                       List oxyData = jsonDecode(resOxy.body);
