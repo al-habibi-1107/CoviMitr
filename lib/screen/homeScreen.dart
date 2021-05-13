309ebb30e2bd7883e01e95715ef1e5d88ed1a897
Aditya <aditya.@Adityas-MacBook-Pro.local>
2021-05-13 12:47:37 +0530

    HomeUi Desktop View

--- lib/screen/homeScreen.dart
+++ lib/screen/homeScreen.dart
@@ -20,7 +20,116 @@ class _HomeScreenState extends State<HomeScreen> {
     return LayoutBuilder(
       builder: (context, constraints) {
         if (constraints.maxWidth > 451) {
-          return Container();
+          return Scaffold(
+            backgroundColor: Color(0xfff6f7f9),
+            appBar: AppBar(
+              backgroundColor: Color(0xfff6f7f9),
+              elevation: 5,
+              title: Text(
+                'Covimitr',
+                style: GoogleFonts.calligraffitti(
+                  color: Colors.purple,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 32,
+                  letterSpacing: 2,
+                ),
+              ),
+              actions: [
+                IconButton(
+                  icon: Icon(
+                    Icons.info_outline,
+                    color: Colors.purple,
+                  ),
+                  onPressed: () {
+                    Navigator.of(context).pushNamed(About.routeName);
+                  },
+                )
+              ],
+            ),
+            body: SafeArea(
+              child: Column(
+                children: [
+                  Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: Text(
+                      'Tap cards to view',
+                      style: GoogleFonts.montserrat(
+                        fontSize: 18,
+                        color: Colors.black54,
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: GridView(
+                      padding: EdgeInsets.symmetric(horizontal: 10),
+                      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+                          maxCrossAxisExtent: deviceSize.width * 0.3,
+                          childAspectRatio: 1.95,
+                          crossAxisSpacing: 0,
+                          mainAxisSpacing: 0),
+                      children: [
+                        HomeTile(
+                          onPressed: () {
+                            Navigator.of(context)
+                                .pushNamed(OxygenScreen.routeName);
+                          },
+                          text: "Oxygen",
+                          deviceSize: deviceSize,
+                          tileCol: Colors.purple.shade100,
+                          imgAdr: "assets/image/O2.png",
+                        ),
+                        HomeTile(
+                          onPressed: () {
+                            Navigator.of(context)
+                                .pushNamed(MedicineScreen.routeName);
+                          },
+                          text: "Medicine",
+                          deviceSize: deviceSize,
+                          tileCol: Colors.blue.shade100,
+                          imgAdr: "assets/image/medicine.png",
+                        ),
+                        HomeTile(
+                          onPressed: () {
+                            Navigator.of(context)
+                                .pushNamed(TestingScreen.routeName);
+                          },
+                          text: "Testing",
+                          deviceSize: deviceSize,
+                          tileCol: Colors.green.shade100,
+                          imgAdr: "assets/image/testing.png",
+                        ),
+                        HomeTile(
+                          onPressed: () {
+                            Navigator.of(context)
+                                .pushNamed(PlasmaScreen.routeName);
+                          },
+                          text: "Plasma",
+                          deviceSize: deviceSize,
+                          tileCol: Colors.teal.shade100,
+                          imgAdr: "assets/image/plasma.png",
+                        ),
+                      ],
+                    ),
+                  ),
+                  Card(
+                    color: Colors.yellow.shade100,
+                    child: Padding(
+                      padding: EdgeInsets.symmetric(
+                          vertical: deviceSize.height * 0.01,
+                          horizontal: deviceSize.width * 0.04),
+                      child: Text(
+                        "DISCLAIMER: All of these resources provided on an “as and when basis” and “as in” based on a fact check done by volunteers who are dedicated to help individuals and families in such challenging times. By using these resources, you are agreeing that COVID FIGHTERS INDIA, however, do not accept any responsibility or liability for the accuracy, content, completeness, legality or reliability of the information contained in any of these.",
+                        style: GoogleFonts.aBeeZee(
+                          color: Colors.deepPurple,
+                        ),
+                        textAlign: TextAlign.justify,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          );
         } else {
           return HomeMobileView(deviceSize: deviceSize);
         }
@@ -127,16 +236,19 @@ class HomeMobileView extends StatelessWidget {
             ),
             Container(
               margin: EdgeInsets.only(bottom: deviceSize.height * 0.03),
-              child: Padding(
-                padding: EdgeInsets.symmetric(
-                    vertical: deviceSize.height * 0.01,
-                    horizontal: deviceSize.width * 0.04),
-                child: Text(
-                  "DISCLAIMER: All of these resources provided on an “as and when basis” and “as in” based on a fact check done by volunteers who are dedicated to help individuals and families in such challenging times. By using these resources, you are agreeing that COVID FIGHTERS INDIA, however, do not accept any responsibility or liability for the accuracy, content, completeness, legality or reliability of the information contained in any of these.",
-                  style: GoogleFonts.aBeeZee(
-                    color: Colors.deepPurple,
+              child: Card(
+                color: Colors.yellow.shade100,
+                child: Padding(
+                  padding: EdgeInsets.symmetric(
+                      vertical: deviceSize.height * 0.01,
+                      horizontal: deviceSize.width * 0.04),
+                  child: Text(
+                    "DISCLAIMER: All of these resources provided on an “as and when basis” and “as in” based on a fact check done by volunteers who are dedicated to help individuals and families in such challenging times. By using these resources, you are agreeing that COVID FIGHTERS INDIA, however, do not accept any responsibility or liability for the accuracy, content, completeness, legality or reliability of the information contained in any of these.",
+                    style: GoogleFonts.aBeeZee(
+                      color: Colors.deepPurple,
+                    ),
+                    textAlign: TextAlign.justify,
                   ),
-                  textAlign: TextAlign.justify,
                 ),
               ),
             ),
